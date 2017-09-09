18b6648f4ea0bd949339ea5e48c0c49c6b35644d
Roberto Butti <roberto.butti@gmail.com>
2017-09-09 17:09:42 +0200

    add asyncdata to products.vue

--- pages/products.vue
+++ pages/products.vue
@@ -1,3 +1,27 @@
 <template>
-  <h1>Products</h1>
+  <h1 class="red">{{ name }}!</h1>
 </template>
+
+<script>
+export default {
+  asyncData (context) {
+    // called every time before loading the component
+    return { name: 'Products' }
+  },
+  fetch () {
+    // The fetch method is used to fill the store before rendering the page
+  }
+  /*,
+  head () {
+    // Set Meta Tags for this Page
+  }
+  */
+  // and more functionality to discover
+}
+</script>
+
+<style>
+.red {
+  color: red;
+}
+</style>
