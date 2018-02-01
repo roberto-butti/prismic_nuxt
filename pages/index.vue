ae8816783005b3cac955b2e7ecda39c405f7ccda
Roberto Butti <roberto.butti@gmail.com>
2018-02-01 21:38:58 +0100

    Added in home nuxt-link to producs page

--- pages/index.vue
+++ pages/index.vue
@@ -9,8 +9,7 @@
         Nuxt.js project with Prismic.io
       </h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+        <nuxt-link to="/products">Products</nuxt-link>
       </div>
     </div>
   </section>
