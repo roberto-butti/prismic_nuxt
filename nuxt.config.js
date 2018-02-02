8df59381e614bc75d277e6b2c1278970d57cb47a
Roberto Butti <roberto.butti@gmail.com>
2018-02-02 07:53:50 +0100

    Playing with page transition

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,9 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  css: [
+    'assets/main.css'
+  ],
   /*
   ** Customize the progress-bar color
   */
